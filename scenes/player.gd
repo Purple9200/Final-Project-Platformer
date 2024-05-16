2585d20e7ba6fbd49180a8d8d58f8f6bc0d23089
dnskvlnko <dekovalenko@s.sfusd.edu>
2024-05-16 12:39:05 -0700

    Player 2.0

--- scenes/player.gd
+++ scenes/player.gd
@@ -7,6 +7,7 @@ const JUMP_VELOCITY = -300.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,11 +15,24 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("Idle") 
+		else:
+			animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("jump")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
